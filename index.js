f06f398e7d52af1bd545e22c364f7736905be3b0
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-01-03 08:50:49 -0800

    Bulk of coding complete, mvp candidate

--- index.js
+++ index.js
@@ -1,13 +1,92 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = ({ projectTitle, projectDescription, projectInstall, projectUsage, projectLicense, projectContrib, projectTest, projectGithub, projectEmail }) =>
+`## Project Title
+${projectTitle}
+## Description
+${projectDescription}
+## Table of Contents
+## Installation
+${projectInstall}
+## Usage
+${projectUsage}
+## License
+This project is covered under the ${projectLicense} license
+## How to Contribute
+${projectContrib}
+## Tests
+${projectTest}
+## Questions
+If you have any questions or would like to contact me, please use one of the two below methods
+http://www.github.com/${projectGithub}
+${projectEmail}`
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      message: "Enter a description for your project",
+      name: "projectDescription",
+    },
+    {
+      type: "input",
+      message: "Enter the installation instructions for your project",
+      name: "projectInstall",
+    },
+    {
+      type: "list",
+      message: "Which license is this project covered under?",
+      name: "projectLicense",
+      choices: ["GPL", "Test", "Another Test"]
+    },
+    {
+        type: "input",
+        message: "How is your project intended to be used?",
+        name: "projectUsage",
+      },
+      {
+        type: "input",
+        message: "Enter instructions on how others may contribute to this project",
+        name: "projectContrib",
+      },
+      {
+        type: "input",
+        message: "Enter any test instructions for the project",
+        name: "projectTest",
+      },
+      {
+        type: "input",
+        message: "Enter your Github user name",
+        name: "projectGithub",
+      },
+      {
+        type: "input",
+        message: "Enter your email address",
+        name: "projectEmail",
+      }
+])
+.then((answer) => {
+    const markdownCode = generateMarkdown(answer);
+    writeToFile("temp.md", markdownCode)
+});
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+function writeToFile(fileName, markdownCode) {
+fs.writeFile(fileName, markdownCode, (err) =>
+err ? console.log(err) : console.log("Success!")
+);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
---
+++ temp.md
@@ -0,0 +1,19 @@
+## Project Title
+Test
+## Description
+asdfasdf
+## Table of Contents
+## Installation
+asdfasdfasdf
+## Usage
+asdfasdf
+## License
+This project is covered under the GPL license
+## How to Contribute
+asdfasdf
+## Tests
+asdfasdf
+## Questions
+If you have any questions or would like to contact me, please use one of the two below methods
+http://www.github.com/stephen-scheiman
+sbscheiman@gmail.com
