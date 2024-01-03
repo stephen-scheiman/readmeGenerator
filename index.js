2e86b9451d4257a4d008f3ecc98952d60d30387a
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-01-03 12:45:33 -0800

    Working on fixing MVP to use modularization

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = ({ projectTitle, projectDescription, projectInstall, projectUsage, projectLicense, projectContrib, projectTest, projectGithub, projectEmail }) =>
-`## Project Title
-${projectTitle}
-## Description
-${projectDescription}
-## Table of Contents
-## Installation
-${projectInstall}
-## Usage
-${projectUsage}
-## License
-This project is covered under the ${projectLicense} license
-## How to Contribute
-${projectContrib}
-## Tests
-${projectTest}
-## Questions
-If you have any questions or would like to contact me, please use one of the two below methods
-http://www.github.com/${projectGithub}
-${projectEmail}`
-
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = inquirer
@@ -45,7 +25,7 @@ const questions = inquirer
       type: "list",
       message: "Which license is this project covered under?",
       name: "projectLicense",
-      choices: ["GPL", "Test", "Another Test"]
+      choices: ["Gnu GPL", "Apache", "BSD", "MIT"]
     },
     {
         type: "input",
@@ -75,7 +55,8 @@ const questions = inquirer
 ])
 .then((answer) => {
     const markdownCode = generateMarkdown(answer);
-    writeToFile("temp.md", markdownCode)
+    console.log(markdownCode);
+    writeToFile("README.md", markdownCode)
 });
 
 // // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(projectLicense) {
+  console.log(projectLicense);
+  if (projectLicense === "Gnu GPL") {
+    const badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (projectLicense === "Apache") {
+    const badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (projectLicense === "MIT") {
+    const badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (projectLicense === "BSD") {
+    const badge = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else {
+    return "";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(projectLicense) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(projectLicense) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+const generateMarkdown = ({
+  projectTitle,
+  projectDescription,
+  projectInstall,
+  projectUsage,
+  projectLicense,
+  projectContrib,
+  projectTest,
+  projectGithub,
+  projectEmail,
+  badge,
+}) => {
+  return `## ${projectTitle} ${badge}
+  ## Description
+  ${projectDescription}
+  ## Table of Contents
+  ## Installation
+  ${projectInstall}
+  ## Usage
+  ${projectUsage}
+  ## License
+  This project is covered under the ${projectLicense} license
+  ## How to Contribute
+  ${projectContrib}
+  ## Tests
+  ${projectTest}
+  ## Questions
+  If you have any questions or would like to contact me, please use one of the two below methods:\n
+  http://www.github.com/${projectGithub}\n
+  ${projectEmail}`;
+};
 
 module.exports = generateMarkdown;
